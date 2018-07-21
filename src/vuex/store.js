97545418cc7dfbe41c59ebd90f1a8e98340bdc8b
Renan Coelho <sayhicoelho@gmail.com>
2018-07-21 02:13:59 -0300

    Add currentUser to Vuex store

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,34 +7,40 @@ Vue.use(Vuex)
 
 const state = {
   firstLoad: true,
-  routing: false
+  routing: false,
+  currentUser: null
 }
 
 const getters = { }
 
 const mutations = {
-  SET_FIRST_LOAD(state) {
+  SET_CURRENT_USER (state, payload) {
+    state.currentUser = payload
+  },
+  SET_FIRST_LOAD (state) {
     state.firstLoad = false
   },
-  SET_ROUTING(state, payload) {
+  SET_ROUTING (state, payload) {
     state.routing = payload
   }
 }
 
 const actions = {
-  FIRST_LOAD({ commit }) {
+  FIRST_LOAD ({ commit }) {
     commit('SET_FIRST_LOAD')
   },
-  ROUTE_PENDING({ commit }) {
+  ROUTE_PENDING ({ commit }) {
     commit('SET_ROUTING', true)
   },
-  ROUTE_COMPLETE({ commit }) {
+  ROUTE_COMPLETE ({ commit }) {
     commit('SET_ROUTING', false)
   },
-  FETCH_AUTH({ state, dispatch }) {
+  FETCH_AUTH ({ state, dispatch, commit }) {
     return new Promise(resolve => {
       if (state.firstLoad) {
         firebase.auth().onAuthStateChanged(user => {
+          commit('SET_CURRENT_USER', user)
+
           if (state.routing) {
             resolve(user)
           } else if (!state.routing && !user) {
