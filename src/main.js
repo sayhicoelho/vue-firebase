ba8d8094ac9bd4728ad53271471e96379ea38209
Renan Coelho <sayhicoelho@gmail.com>
2018-07-21 00:08:39 -0300

    Setup vee-validate

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
+Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 new Vue({
