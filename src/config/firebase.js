c2b7c6a9383401995274805493694e88facf5c21
Renan Coelho <sayhicoelho@gmail.com>
2018-07-21 00:18:45 -0300

    Setup Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
+
+export default firebase
